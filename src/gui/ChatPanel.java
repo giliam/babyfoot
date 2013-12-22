91a8dc43028a5ef5480ba7e2f270617ebb6748f6
giliam <matthieu.denoux@gmail.com>
2013-12-23 00:08:53 +0100

    Ajout des onglets et préparation pour le tchat

--- src/gui/ChatPanel.java
+++ src/gui/ChatPanel.java
@@ -1,5 +1,5 @@
 package gui;
-
+import core.Chat;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -7,8 +7,37 @@ import javax.swing.*;
 
 public class ChatPanel extends JPanel implements ActionListener {
 	MainFrame window;
+	JButton push;
+	JTabbedPane onglet;
 	public ChatPanel(MainFrame f) {
+		Chat c = new Chat();
+		
 		window = f;
+		
+		//Création de plusieurs Panneau
+		JPanel[] tPan = { new JPanel(), new JPanel() };
+		tPan[0].setBackground(Color.BLACK);
+	    //Création de notre conteneur d'onglets
+		onglet = new JTabbedPane();
+	    onglet.add("Chat", tPan[0]);
+	    onglet.add("Salons", tPan[1]);
+	    onglet.setPreferredSize(new Dimension(300,800));
+	    
+		JTextPane text = new JTextPane();
+		text.setText(c.getText() + c.getText());
+		
+		JScrollPane displayZone = new JScrollPane(text);
+		displayZone.setPreferredSize(new Dimension(280,500));
+		
+		JTextField textfield = new JTextField();
+		textfield.setPreferredSize(new Dimension(180,20));
+		push = new JButton("Envoyer");
+		push.setPreferredSize(new Dimension(90,25));
+		
+		add(onglet);
+		tPan[0].add(displayZone);
+		tPan[0].add(textfield);
+		tPan[0].add(push);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
