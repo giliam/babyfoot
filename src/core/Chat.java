e399c1fb63e8bad7976cadfe5865ec65394aa303
giliam <matthieu.denoux@gmail.com>
2013-12-23 01:53:02 +0100

    Rajout des fichiers de structure

---
+++ src/core/Chat.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Chat {
+	public String getText(){
+		return "Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. \n" +
+				"Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. \n" +
+				"Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. \n" +
+				"Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. \n" +
+				"Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. Lorem ipsum ipsa facto est. \n";
+	}
+}
