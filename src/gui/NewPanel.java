9bb4fa1dca3f79f9d19d6cf39ac3c0cfcf48ffd1
giliam <matthieu.denoux@gmail.com>
2014-01-03 20:28:52 +0100

    Début de la gestion de partie. TODO : paramètres de parties et gestion dans la db

--- src/gui/NewPanel.java
+++ src/gui/NewPanel.java
@@ -9,29 +9,51 @@ import core.Main;
 
 @SuppressWarnings("serial")
 public class NewPanel extends BPanel implements ActionListener {
-	JButton bQuit;
-	JButton bGo;
-	JButton bReturn;
+	private JButton bQuit = new JButton("Quitter");
+	private JButton bGo = new JButton("Ouvrir la partie à d'autres joueurs");
+	private JButton bReturn = new JButton("Retour");
+	private JRadioButton bNumber1 = new JRadioButton("1 contre 1");
+	private JRadioButton bNumber2 = new JRadioButton("2 contre 2");
+	private ButtonGroup bNumberPlayers = new ButtonGroup();
+
 	public NewPanel(MainFrame f) {
 		super(f);
 		
-		bReturn = new JButton("Retour");
-		bGo = new JButton("Ouvrir la partie à d'autres joueurs");
-	    bQuit = new JButton("Quitter");
-	    
 	    ChatPanel chat = new ChatPanel();
 		chat.setBackground(Color.BLACK);
 		chat.setPreferredSize(new Dimension(300,700));
 		chat.setMinimumSize(new Dimension(300,700));
 		add(chat,BorderLayout.EAST);
 		
+		bNumber1.setPreferredSize(new Dimension(500,100));
+		bNumber1.setBackground(Color.ORANGE);
+		bNumber2.setPreferredSize(new Dimension(500,100));
+		bNumber2.setBackground(Color.ORANGE);
+		
 		JPanel menu = new JPanel();
+		menu.setLayout(new GridLayout());
 		menu.setBackground(Color.ORANGE);
 		add(menu,BorderLayout.CENTER);
 		
-		menu.add(bReturn);
-		menu.add(bGo);
-		menu.add(bQuit);
+		bNumber1.setSelected(true);
+	    //On ajoute les boutons au groupe
+		bNumberPlayers.add(bNumber1);
+		bNumberPlayers.add(bNumber2);
+	    
+		Box buttonsPlayers = Box.createHorizontalBox();
+		buttonsPlayers.add(new JLabel("Nombre de joueurs : "));
+		buttonsPlayers.add(bNumber1);
+		buttonsPlayers.add(bNumber2);
+		Box buttonsBottom = Box.createHorizontalBox();
+	    buttonsBottom.add(bReturn);
+	    buttonsBottom.add(bGo);
+	    buttonsBottom.add(bQuit);
+	    //On crée un conteneur avec gestion verticale
+	    Box menuContent = Box.createVerticalBox();
+	    menuContent.add(buttonsPlayers);
+	    menuContent.add(buttonsBottom);
+	    menu.add(menuContent);
+	    
 		
 		bReturn.addActionListener(this);
 		bGo.addActionListener(this);
