ff1a987e7399685734b8eb8663677fbdb07d9eba
giliam <matthieu.denoux@gmail.com>
2014-01-17 18:20:40 +0100

    Documente BPanel.java

--- client/src/gui/BPanel.java
+++ client/src/gui/BPanel.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
+/** Cette classe abstraite permet simplement de garder quelques données communes à tous les panels qui seront utilisés dans l'IHM
+ par la suite : le LayoutManager qui est un BorderLayout, la couleur du fond (blanche) et la présence du header (cf. @Header) dans la zone NORTH
+ du BorderLayout. */
 public abstract class BPanel extends JPanel {
 	MainFrame window;
 	public BPanel(MainFrame f){
